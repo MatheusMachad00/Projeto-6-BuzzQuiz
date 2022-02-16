f808511a57850b82505f3895d9f3524129aaa3d3
Vinicios Pires <contato.viniciospires@gmail.com>
2022-02-16 09:37:15 -0300

    Todas telas finalizadas

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+function abrirTela2(quizEscolhido) {
+    const tela1 = document.querySelector(".tela-1");
+    tela1.classList.add("off");
+
+    const tela2 = document.querySelector(".tela-2");
+    tela2.classList.add("on")
+}
